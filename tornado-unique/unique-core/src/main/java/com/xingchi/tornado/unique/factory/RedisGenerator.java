9c5d27718c57a5f4203fbe051235b8cfc4d2ba44
xingchi <1710518450@qq.com>
2023-05-03 18:09:10 +0800

    结构提交

---
+++ tornado-unique/unique-core/src/main/java/com/xingchi/tornado/unique/factory/RedisGenerator.java
@@ -0,0 +1,17 @@
+package com.xingchi.tornado.unique.factory;
+
+/**
+ * Redis生成器
+ *
+ * @author xingchi
+ * @date 2023/5/3 10:18
+ * @modified xingchi
+ */
+public class RedisGenerator implements IDGGenerator<Long>{
+
+    @Override
+    public Long nextId() {
+        return null;
+    }
+
+}
