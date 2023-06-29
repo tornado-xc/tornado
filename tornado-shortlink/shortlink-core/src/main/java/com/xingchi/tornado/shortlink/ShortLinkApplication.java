5b65c3b5f76222e8d7f6042bf0907b7ec8bf280d
xiaoya <xiaoya@libawall.cn>
2023-06-29 17:51:54 +0800

    消息id细节调整

---
+++ tornado-shortlink/shortlink-core/src/main/java/com/xingchi/tornado/shortlink/ShortLinkApplication.java
@@ -0,0 +1,19 @@
+package com.xingchi.tornado.shortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 短链系统主启动
+ *
+ * @author xiaoya
+ * @date 2023/6/29
+ */
+@SpringBootApplication
+public class ShortLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkApplication.class, args);
+    }
+
+}
